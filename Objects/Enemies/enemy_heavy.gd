85e8fd254a8f0ae40d3df756cb4158d24fe44c1f
Marcelo Moreira <marcelo.lm91@gmail.com>
2025-03-21 16:56:17 -0300

    Adicionados arquivos de inimigos e projeteis que faltaram no ultimo commit

---
+++ Objects/Enemies/enemy_heavy.gd
@@ -0,0 +1,19 @@
+extends Area2D
+#@onready var sprite: AnimatedSprite2D = $Sprite
+@onready var attack_timer: Timer = $AttackTimer
+@export var projectile : PackedScene
+
+func _ready() -> void:
+	attack_timer.start()
+	set_modulate(Color(0, 1, 0, 1))
+	pass
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group('Projectiles') and area.is_deflected:
+		queue_free()
+
+#funcao para atirar a cada X segundos
+func _on_attack_timer_timeout() -> void:
+	var p = projectile.instantiate()
+	add_child(p)
+	
