11785de5b90170de2ac7f7036fbee0cb277b2710
Marcelo Moreira <marcelo.lm91@gmail.com>
2025-03-22 19:00:01 -0300

    adicionados os primeiros sons e o framework de audio

--- Objects/Enemies/enemy.gd
+++ Objects/Enemies/enemy.gd
@@ -2,17 +2,18 @@ extends Area2D
 
 @onready var attack_timer: Timer = $AttackTimer
 @onready var bullet = preload("res://Objects/Enemies/bullet.tscn")
-
+@export var audio_stream_player: AudioStreamPlayer
 @export var bullet_speed:float = 400
 @export var damage:int = 1
+@export var explosao:AudioStream
+@export var tiro:AudioStream
 
 func _ready() -> void:
 	attack_timer.start()
 	pass
 
-
-
 func fire_bullet(_origin, _speed = null, _damage = null):
+	Globals.new().tocar_audio(audio_stream_player, tiro, -10, 0.5, 1.4)
 	var b = bullet.instantiate()
 	if _speed  != null: b.speed = _speed
 	if _damage != null: b.damage = _damage
@@ -21,11 +22,13 @@ func fire_bullet(_origin, _speed = null, _damage = null):
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group('Bullet') and area.is_deflected:
-		queue_free()
-		
+		area.queue_free()
+		queue_free() # mudar para dano causado?
+
 # funcao para atirar a cada X segundos
 func _on_attack_timer_timeout() -> void:
 	var b = fire_bullet(global_position, bullet_speed, damage)
 	add_sibling(b)
-	
-	
+
+func _on_tree_exited() -> void:
+	Globals.new().tocar_audio(audio_stream_player, explosao, 5, 0.8, 1.2)
