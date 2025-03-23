b0b0c69a849b8045cadec5a71d31f86f544440ca
gilgadev <gilgadev@gmail.com>
2025-03-23 00:07:38 +0000

    Add level datatype and spawn logic.

--- Objects/Enemies/enemy.gd
+++ Objects/Enemies/enemy.gd
@@ -6,8 +6,8 @@ extends Area2D
 
 @export var bullet_speed:float = 400
 @export var damage:int = 1
-@export var explosao:AudioStream
-@export var tiro:AudioStream
+@export var explosion_sfx:AudioStream
+@export var shot_sfx:AudioStream
 @export var bullet_color:Color
 
 func _ready() -> void:
@@ -15,8 +15,8 @@ func _ready() -> void:
 	pass
 
 func fire_bullet(_origin, _speed = null, _damage = null):
-	Globals.new().tocar_audio(audio_stream_player, tiro, -10, 0.5, 1.4)
-	var b = bullet.instantiate()
+	Globals.new().play_audio(audio_stream_player, shot_sfx, -10, 0.5, 1.4)
+	var b = bullet.instantiate() # b is the instance of the bullet
 	if _speed  != null: b.speed = _speed
 	if _damage != null: b.damage = _damage
 	b.global_position = _origin
@@ -28,10 +28,13 @@ func _on_area_entered(area: Area2D) -> void:
 		area.queue_free()
 		queue_free() # mudar para dano causado?
 
+func move_to(_to:Vector2, _duration:float):
+	print("Moving from {0} to {1} in {2} miliseconds".format([global_position, _to, _duration]))
+
 # funcao para atirar a cada X segundos
 func _on_attack_timer_timeout() -> void:
 	var b = fire_bullet(global_position, bullet_speed, damage)
 	add_sibling(b)
 
 func _on_tree_exited() -> void:
-	Globals.new().tocar_audio(audio_stream_player, explosao, 5, 0.8, 1.2)
+	Globals.new().play_audio(audio_stream_player, explosion_sfx, 5, 0.8, 1.2)
--- Objects/Levels/main_scene.gd
+++ Objects/Levels/main_scene.gd
@@ -3,14 +3,103 @@ class_name Levels
 @onready var globals : Globals
 @onready var player = $Hero
 @onready var hud = $CanvasGroup/hud
+@onready var spawn_timer: Timer = $SpawnTimer
 @onready var grid: AnimatedSprite2D = $Grid
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @export var grid_speed: float
 
+const ENEMY_LIGHT = preload("res://Objects/Enemies/enemy_light.tscn")
+const ENEMY_HY = preload("res://Objects/Enemies/enemy_heavy.tscn")
+const OBSTACLE_1 = preload("res://Objects/Obstacles/obstacle1.tscn")
+const OBSTACLE_2 = preload("res://Objects/Obstacles/obstacle2.tscn")
+const OBSTACLE_3 = preload("res://Objects/Obstacles/obstacle3.tscn")
+
+# Level vars
+var level_index = 0 
+var level
+
+var level1 = [
+	{
+	"spawn_type": "enemy", 
+	"enemy_type": "light", 
+	"next_spawn_delay": 5.0,
+	"from": Vector2(200, 200),
+	"to": Vector2.ZERO,
+	"duration": 0.0
+	},
+	{
+	"spawn_type": "enemy", 
+	"enemy_type": "heavy", 
+	"next_spawn_delay": 5.0,
+	"from": Vector2(500, 200),
+	"to": Vector2.ZERO,
+	"duration": 0.0
+	}
+]
+"""
+Expected dictionary for obstacles:
+	{
+		"spawn_type": "obstacle", 
+		"obstacle": 1, 
+		"next_spawn_delay: 1.0,
+		"pos_3d": Vector2(40, 50),
+		"z" : 500
+	}
+"""
+var level2 = {
+	
+}
+var level3 = {
+	
+}
+
+var levels = [
+	level1, level2, level3
+]
+
 func _ready() -> void:
+	level = levels[level_index]
+	spawn_timer.wait_time = 1
+	spawn_timer.start()
 	pass
 
-func _physics_process(_delta: float) -> void:
+func create_enemy(spawn:Dictionary):
+	var _enemy 
+	match spawn.enemy_type:
+		"light": 
+			_enemy = ENEMY_LIGHT
+		"heavy":
+			_enemy = ENEMY_HEAVY
+	
+	var e = _enemy.instantiate()
+	e.global_position = spawn.from
+	e.move_to(spawn.to, spawn.duration)
+	add_child(e)
+
+func create_obstacle(spawn:Dictionary):
+	var _obstacle
+	match spawn.obstacle:
+		1:
+			_obstacle = OBSTACLE_1
+		2:
+			_obstacle = OBSTACLE_2
+		3:
+			_obstacle = OBSTACLE_3
+
+	var o:Obstacle = _obstacle.instantiate()
+	o.pos_3d = spawn.pos_3d
+	o.z = spawn.z
+	add_child(o)
+
+func _on_spawn_timer_timeout() -> void:
+	if level.is_empty(): return
+	var current_enemy = level.pop_front()
+	create_enemy(current_enemy)
+	var wait_time = current_enemy.next_spawn_delay
+	spawn_timer.start(wait_time)
+
+func _process(_delta: float) -> void:
+	
 	hud.update_health_label(player.life)
 	hud.update_boost_label(player.boosts)
 	_text_alert_update(_delta)
